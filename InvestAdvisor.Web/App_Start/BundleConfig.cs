19cc773ed978889f5b9500fc504cab0d85568097
volchan <volchan@outlook.com>
2017-10-06 00:04:48 +0300

    start

--- InvestAdvisor.Web/App_Start/BundleConfig.cs
+++ InvestAdvisor.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace InvestAdvisor.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,6 +21,7 @@ namespace InvestAdvisor.Web
 
             bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
                        "~/Scripts/popper.js"));
+                       //,"~/Scripts/jquery.easing.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
