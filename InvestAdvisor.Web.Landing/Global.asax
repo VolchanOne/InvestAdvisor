60083d8d99172e1294ddc374a7351eefe93e0462
volchan <volchan@outlook.com>
2017-12-03 21:56:39 +0200

    landing

---
+++ InvestAdvisor.Web.Landing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InvestAdvisor.Web.Landing.MvcApplication" Language="C#" %>
---
+++ InvestAdvisor.Web.Landing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InvestAdvisor.Web.Landing.Startup))]
+namespace InvestAdvisor.Web.Landing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
